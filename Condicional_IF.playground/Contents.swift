ef2ab354fbb68f7b20f61bcc73e3f96972d6ecc8
mvalbuquerque <albuquerquenet@icloud.com>
2016-11-05 22:25:47 -0200

    Condicional

---
+++ Condicional_IF.playground/Contents.swift
@@ -0,0 +1,82 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+//Declarando a variável salário
+let salario : Float = 8000;
+
+//Declarando a variável conta
+let contas :  Float = 8000;
+
+var carro1 = "Carro1"
+var carro2 = "Carro2"
+
+
+
+
+if (salario > contas)
+    
+
+{
+
+print("Ufa que bom")
+
+}
+else
+
+{
+print("Deu Ruim")
+
+}
+
+
+if (salario < contas)
+
+{
+
+print("Deu Ruim")
+
+}
+
+else
+{
+
+print("Ufa que bom")
+
+}
+
+
+if (salario == contas)
+    
+{
+    
+    print("Deu Ruim")
+    
+}
+    
+else
+{
+    
+    print("Ufa que bom")
+    
+}
+
+
+if (carro1 == carro2)
+
+{
+
+print("As variáveis são iguais")
+}
+else
+
+{
+print("As variáveis são diferentes")
+
+}
+
+
